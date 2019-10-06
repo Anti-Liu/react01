1415ba92c05916d048225993f5762ed43e352702
刘春霞 <liuchunxia@sansi.com>
2019-10-06 09:35:26 +0800

    border color

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'antd';
 import '../css/login.css'
 
 class Login extends React.Component {
@@ -8,10 +9,45 @@ class Login extends React.Component {
             
         }
     }
+
+    divFocus = () => {
+        const divB = document.getElementById('inputDiv');
+        divB.style['border-color'] = 'red';
+    }
+
+
+
+    inputFocus = (e)=>{
+        // const divB = document.getElementById('inputDiv');
+        // console.log(divB)
+        // divB.style['border-color'] = 'black';
+    }
+
+    divBlur = () => {
+        const divB = document.getElementById('inputDiv');
+        divB.style['border-color'] = 'rgb(217, 217, 217)';
+    }
+
     render() {
         return <div className="login-box">
-            <div><img src={process.env.PUBLIC_URL + '/images/user.png'} alt="nihao" className="icon"></img><input type="text" placeholder="Username" /></div>
-            <div><img src={process.env.PUBLIC_URL + '/images/user.png'} alt="nihao" className="icon"></img><input type="text" placeholder="Password" /></div>
+            <div id="inputDiv" 
+                onFocus={this.divFocus}
+                obBlur={this.divBlur}
+                >
+                <Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }}></Icon>
+                <input type="text" 
+                    placeholder="Username"
+                    // onFocus={this.inputFocus}
+                    // onBlur={this.inputBlur}    
+                />
+            </div>
+            <div>
+                <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }}></Icon>
+                <input type="text" 
+                    placeholder="Password"
+
+                />
+            </div>
         </div>
     }
 }
